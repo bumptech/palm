1b432164d805c5a30c97ee3c602bf61029263dd3
Christian Wyglendowski <christian@bu.mp>
2012-03-08 13:03:01 -0500

    Set zip_safe=False.
    This fixes some crazy circular import stuff I was seeing. The zip import
    machinery was trying to import subpackages that imported this packages
    and that was blowing up.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
         packages=["palm", "palm.palmc", ],
         ext_modules=[Extension("palm.palm", sources=sourcefiles)],
         install_requires=["simpleparse >=2.1"],
+        zip_safe=False, # This fixes some crazy circular import junk.
         entry_points='''
         [console_scripts]
         palmc = palm.palmc.main:run
