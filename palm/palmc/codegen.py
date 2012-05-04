9e1e6ec5831ca4623f121cd8f0abb91c398f0634
Michael Schurter <schmichael@urbanairship.com>
2012-05-04 13:07:40 -0700

    Refactor palmc/codegen.py and fix repeated __contains__ methods
    Fixes bumptech/palm#9

--- palm/palmc/codegen.py
+++ palm/palmc/codegen.py
@@ -1,30 +1,45 @@
-import sys
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+import operator as op
 import os
 from palm.palm import ProtoBase
-pfx = ''
-o = []
-def convert_proto_name(n):
-    n = os.path.basename(n)
-    p, last = n.rsplit(".", 1)
+
+
+def convert_proto_name(filename):
+    """Given a .proto filename, return the corresponding palm module name"""
+    filename = os.path.basename(filename)
+    p, last = filename.rsplit(".", 1)
     assert last == "proto"
     return "%s_palm" % p
 
-def gen_module(messages, imports, tlenums):
-    global pfx
-    global o
-    pfx = ''
 
-    out('from palm.palm import ProtoBase, is_string, RepeatedSequence, ProtoValueError\n\n_PB_type = type\n_PB_finalizers = []\n\n')
-    for i in imports:
-        out('from %s import *\n' % convert_proto_name(i))
+class CodeGenerator(object):
+    def __init__(self):
+        self.output = StringIO.StringIO()
+        self.prefix = ''
 
-    for ename, espec in tlenums:
-        write_enum(ename, espec)
+    def gen_module(self, messages, imports, tlenums):
+        self.prefix = ''
 
-    for n, (fields, subs, en) in messages:
-        write_class(n, '', fields, subs, en)
+        self.out("from palm.palm import "
+            "ProtoBase, is_string, RepeatedSequence, ProtoValueError\n")
 
-    out('''
+        for i in imports:
+            self.out('from %s import *\n' % convert_proto_name(i))
+
+        self.out("\n")
+        self.out("_PB_type = type\n")
+        self.out("_PB_finalizers = []\n")
+
+        for ename, espec in tlenums:
+            self.write_enum(ename, espec)
+
+        for n, (fields, subs, en) in messages:
+            self.write_class(n, '', fields, subs, en)
+
+        self.out('''
 
 for cname in _PB_finalizers:
     eval(cname)._pbf_finalize()
@@ -32,36 +47,27 @@ for cname in _PB_finalizers:
 del _PB_finalizers
 ''')
 
-    all = ''.join(o)
+        out = self.output.getvalue()
 
-    o = []
-    return all
+        self.output = StringIO.StringIO()
+        return out
 
-def out(s):
-    global o
-    s = pfx + s.replace("\n", "\n" + pfx)
-    o.append(s)
+    def out(self, s):
+        s = self.prefix + s.replace("\n", "\n" + self.prefix)
+        self.output.write(s)
 
-def clean(name):
-    return name.split('-', 1)[1]
+    def clean(self, name):
+        return name.split('-', 1)[1]
 
-def write_enum(name, spec):
-    out(
-'''
-# Enumeration: %s
-''' % name)
-    for cn, value in sorted(spec.items(), key=lambda (k, v): v):
-        out('''
-%s = %s\n''' % (cn, value))
-    out('''
-TYPE_%s = ProtoBase.TYPE_int32
-'''
-% name)
+    def write_enum(self, name, spec):
+        self.out("\n# Enumeration: %s\n" % name)
+        for cn, value in sorted(spec.items(), key=op.itemgetter(1)):
+            self.out("%s = %s\n" % (cn, value))
+        self.out("\nTYPE_%s = ProtoBase.TYPE_int32\n" % name)
 
-def write_class(name, scope, fields, subs, enums):
-    global pfx
-    name = clean(name)
-    out(
+    def write_class(self, name, scope, fields, subs, enums):
+        name = self.clean(name)
+        self.out(
 '''
 class %s(ProtoBase):
     def __init__(self, _pbf_buf='', _pbf_parent_callback=None, **kw): 
@@ -88,6 +94,9 @@ class %s(ProtoBase):
     _pbf_strings = []
     _pbf_finalizers = []
 
+    def __contains__(self, item):
+        return getattr(self, '%%s__exists' %% item, False)
+
     def __str__(self):
         return '\\n'.join('%%s: %%s' %% (f, repr(getattr(self, '_get_%%s' %% f)())) for f in self.fields()
                           if getattr(self, '%%s__exists' %% f))
@@ -96,56 +105,56 @@ class %s(ProtoBase):
        dict((name, num) for num, (_, _, name, _) in fields.iteritems()),
        "', '".join(name for _, _, name, _ in fields.values())))
 
-    ns = {}
-    for ename, espec in enums.iteritems():
-        pfx += "    "
-        write_enum(ename, espec)
-        pfx = pfx[:-4]
-        ns[ename] = 'enum'
+        ns = {}
+        for ename, espec in enums.iteritems():
+            self.prefix += "    "
+            self.write_enum(ename, espec)
+            self.prefix = self.prefix[:-4]
+            ns[ename] = 'enum'
 
-    next_scope = name if not scope else ".".join([scope, name])
-    for sn, (sf, ss, sens) in subs:
-        pfx += "    "
-        write_class(sn, next_scope, sf, ss, sens)
-        pfx = pfx[:-4]
-        snm = clean(sn)
-        ns[snm] = 'message'
-        out(
+        next_scope = name if not scope else ".".join([scope, name])
+        for sn, (sf, ss, sens) in subs:
+            self.prefix += "    "
+            self.write_class(sn, next_scope, sf, ss, sens)
+            self.prefix = self.prefix[:-4]
+            snm = self.clean(sn)
+            ns[snm] = 'message'
+            self.out(
 '''
     TYPE_%s = %s
 '''  % (snm, snm))
 
-    # TODO -- submessages
-    for num, field in fields.iteritems():
-        write_field(name, scope, num, field, ns)
+        # TODO -- submessages
+        for num, field in fields.iteritems():
+            self.write_field(name, scope, num, field, ns)
 
-    out('''
+        self.out('''
 TYPE_%s = %s
 _PB_finalizers.append('%s%s')
 ''' % (name, name, scope + '.' if scope else '', name))
 
-def write_field_get(num, type, name, default, scope):
-    if default is not None:
-        r = '''
+    def write_field_get(self, num, type, name, default, scope):
+        if default is not None:
+            r = '''
             try:
                 r = self._buf_get(%s, %sTYPE_%s, '%s')
             except:
                 r = ''' + str(default)
-    else:
-        r = '''
+        else:
+            r = '''
             r = self._buf_get(%s, %sTYPE_%s, '%s')'''
-    return r % (num, scope, type, name)
+        return r % (num, scope, type, name)
 
-def write_field(cname, parent, num, field, parent_ns):
-    req, type, name, default = field
-    if hasattr(ProtoBase, 'TYPE_%s' % type):
-        scope = 'ProtoBase.'
-    elif type in parent_ns:
-        scope = '%s.' % (cname if not parent else ".".join([parent, cname]))
-    else:
-        scope = ''
-    if req == 'repeated':
-        out(
+    def write_field(self, cname, parent, num, field, parent_ns):
+        req, type, name, default = field
+        if hasattr(ProtoBase, 'TYPE_%s' % type):
+            scope = 'ProtoBase.'
+        elif type in parent_ns:
+            scope = '%s.' % (cname if not parent else ".".join([parent, cname]))
+        else:
+            scope = ''
+        if req == 'repeated':
+            self.out(
 '''
     class Repeated_%s(RepeatedSequence): 
         class pb_subtype(object):
@@ -161,15 +170,15 @@ def write_field(cname, parent, num, field, parent_ns):
         custom_subtype = scope != 'ProtoBase.'
         scope = 'self.'
         if custom_subtype:
-            out(
+            self.out(
 '''
     @property
     def %s__stream(self):
         return self._get_repeated(%s, self.TYPE_%s, "%s", lazy=True)
 ''' % (name, num, type, name))
 
-    # Back to all fields...
-    out(
+        # Back to all fields...
+        self.out(
 '''
     def _get_%(name)s(self):
         if %(num)s in self._cache:
@@ -222,12 +231,6 @@ def write_field(cname, parent, num, field, parent_ns):
     def %(name)s__exists(self):
         return %(num)s in self._mods or self._buf_exists(%(num)s)
 
-    def __contains__(self, item):
-        try:
-            return getattr(self, '%%s__exists' %% item)
-        except AttributeError:
-            return False
-
     @property
     def %(name)s__type(self):
         return %(scope)sTYPE_%(type)s
@@ -243,10 +246,10 @@ def write_field(cname, parent, num, field, parent_ns):
     _pbf_finalizers.append(_finalize_%(name)s)
 
 ''' % {
-    'name':name, 
-    'num':num, 
-    'field_get':write_field_get(num, type, name, default, scope), 
-    'type':type,
-    'scope':scope,
-    'scopeclass': 'cls.' + scope.split('.', 1)[-1] if scope.startswith('self.') else scope,
-    })
+        'name': name,
+        'num': num,
+        'field_get': self.write_field_get(num, type, name, default, scope),
+        'type': type,
+        'scope': scope,
+        'scopeclass': 'cls.' + scope.split('.', 1)[-1] if scope.startswith('self.') else scope,
+        })
