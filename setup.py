13e013f2ae4d09d7b715eb58f6e606fb82dd7083
Will Moss <wmoss@bu.mp>
2013-02-08 13:48:04 -0800

    Bump the version number
    * Also remove the download url, since it's not valid any more

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ sourcefiles = ["palm/palm.c", "palm/palmcore.c"]
 
 setup(
         name="palm",
-        version="0.1.2",
+        version="0.1.3",
         packages=["palm", "palm.palmc", ],
         ext_modules=[Extension("palm.palm", sources=sourcefiles)],
         install_requires=["simpleparse >=2.1"],
@@ -14,7 +14,6 @@ setup(
         palmc = palm.palmc.main:run
         ''',
         url="https://github.com/bumptech/palm",
-        download_url="http://bu.mp/static/oss/palm/palm-0.1.2.tar.gz",
         description="Fast Protocol Buffer library for Python",
         long_description='''
 palm - Protobufs Are Lightweight Messages
@@ -28,7 +27,7 @@ palm was to make improvements to the Google reference implementation, namely:
  * Clean up the API
 
 While the palm developers feel that the second goal has been achieved, the
-first goal undoubtely has: palm is 40-60x faster than the pure-python 
+first goal undoubtely has: palm is 40-60x faster than the pure-python
 protobuf-2.3 library.
 
 Palm was developed for use at bu.mp .
