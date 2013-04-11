ce4dd14d44e01aef3df125492fd42a689a7412af
Justin Bailey <justin.bailey@urbanairship.com>
2013-04-11 12:58:12 -0700

    Code generation updates; code cleanup
    When generating code for a given proto file, only package definitions for
    files imported by that file will be passed to the 'gen_module' function.
    
    Printing improvements.

--- palm/palmc/main.py
+++ palm/palmc/main.py
@@ -33,56 +33,67 @@ def run():
     d = args[0]
     od = args[1]
     exit_status = 0
-    namespaces = {}
+    packages_to_files = {}
+    files_to_packages = {}
     parsed = []
     protos = [f for f in os.listdir(d) if f.endswith(".proto")]
 
     try:
+        sys.stdout.write("Parsing...\n")
         for p in protos:
             sys.stdout.write(("%s..." % p).ljust(70))
             sys.stdout.flush()
             
             source = open(os.path.join(d, p)).read()
+            _, res, l = make_parser().parse(source)
 
-            parser = make_parser()
-            r = parser.parse(source)
-
-            _, res, l = r
             if l != len(source):
                 raise SyntaxError("Syntax error on line %s near %r" % (
                     source[:l].count('\n') + 1,
                     source[l:l+10]))
-            package = [m for m in res if type(m) is Package]
-            if len(package) == 1:
-                namespaces[package[0].name] = Namespace(package[0].name, p)
-                package = package[0].name
-                # print "added namespace"
-            elif len(package) > 1:
+
+            package_list = [m for m in res if type(m) is Package]
+            if len(package_list) == 1:
+                package = package_list[0].name
+                ns = Namespace(package, p)
+                packages_to_files[package] = ns
+                files_to_packages[p] = ns
+            elif len(package_list) > 1:
                 raise SyntaxError("Proto file %s declares more than 1 package." % p)
             else:
                 package = None
 
             parsed.append((res, package, p))
+            sys.stdout.write("[OKAY]\n")
 
+        sys.stdout.write("Generating code...\n")
         for (res, package, p) in parsed:
             sys.stdout.write(("%s..." % p).ljust(70))
             sys.stdout.flush()
+            # Filter the package mappings given to 
+            # gen_module based on the packages 
+            # imported by this proto file. 
+            #
+            # Fortunately, imports are not transitive (except 'import public'
+            # but we aren't going to worry abou that), so we only need
+            # to look for packages imported directly.
+            imports = [m for m in res if type(m) is str]
+            module_packages = dict([(package, file) for (package, file) 
+                                   in packages_to_files.iteritems() 
+                                   if package in imports])
             s = gen_module([m for m in res if type(m) is tuple],
-                    [m for m in res if type(m) is str],
+                    imports,
                     [m for m in res if type(m) is list],
                     options.with_slots,
-                    namespaces,
+                    module_packages,
                     package
                     )
             open(os.path.join(od, convert_proto_name(p) + ".py"), 'wb').write(s)
+            sys.stdout.write("[OKAY]\n")
     except:
         sys.stdout.write("[FAIL]\n")
-        sys.stdout.flush()
         traceback.print_exc()
         exit_status = 1
-    else:
-        sys.stdout.write("[OKAY]\n")
-        sys.stdout.flush()
 
-    print "namespaces: " + namespaces.__str__()
+    sys.stdout.flush()
     return exit_status
